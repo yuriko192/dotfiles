0894b295f10533c699b962a49ba13cf77e2d4b7b
yuriko192 <ivanwijaya19@yahoo.co.id>
2026-09-02 23:34:10 +0700

    add hyprland config

---
+++ hypr/.config/hypr/config/monitors.lua
@@ -0,0 +1,21 @@
+-- Monitor wiki https://wiki.hypr.land/Configuring/Basics/Monitors/
+-- Example: output can be found with hyprctl monitors. Edit variables.lua for the monitor outputs instead of here directly
+-- hl.monitor({
+--     output    = "MONITOR1",
+--     mode      = "1920x1080@60",
+--     position  = "0x0",
+--     scale     = "1",
+-- })
+
+hl.monitor({
+  output = MONITOR1,
+  mode = "preferred",
+  position = "auto-left",
+  scale = "auto",
+})
+hl.monitor({
+  output = MONITOR2,
+  mode = "preferred",
+  position = "auto-right",
+  scale = "auto",
+})
